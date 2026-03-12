1c70775880d3463550acd7931694c4284ae9c2be
pritiborude <borudepriti571@gmail.com>
2026-03-13 01:26:49 +0530

    Updated homepage layout, fixed CSS duplication, added video section

--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -17,3 +17,36 @@ panel.classList.remove("expand");
 });
 
 });
+// VIDEO SWITCH
+
+const video = document.getElementById("riffVideo");
+
+if(video){
+
+const source = video.querySelector("source");
+
+const videos = [
+"../assets/video/musicvid1.mp4",
+"../assets/video/musicvid2.mp4"
+];
+
+let index = 0;
+
+setInterval(() => {
+
+video.classList.add("fade");   // start fade out
+
+setTimeout(() => {
+
+index = (index + 1) % videos.length;
+
+source.src = videos[index];
+video.load();
+
+video.classList.remove("fade");  // fade back in
+
+},1200); // fade duration
+
+},12000); // time before switching video
+
+}
